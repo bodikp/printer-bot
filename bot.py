88c0e9fe466a9cd38ade6dbf6f9a6326b891c958
bodikp <peterb@microsoft.com>
2016-04-07 21:24:08 -0700

    pretty printing

--- bot.py
+++ bot.py
@@ -21,7 +21,8 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+#SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly','https://www.googleapis.com/auth/gmail.labels'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'botclient'
 
@@ -40,7 +41,7 @@ def get_credentials():
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+                                   'gmail-python-printerbot.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
